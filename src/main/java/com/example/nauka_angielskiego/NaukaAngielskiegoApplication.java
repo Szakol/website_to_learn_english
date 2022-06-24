ad661b786872370f7548b7a959ee1344e7c2fe5d
Kacper Piątek <kapistreo@gmail.com>
2022-06-24 16:43:16 +0200

    initial commit

---
+++ src/main/java/com/example/nauka_angielskiego/NaukaAngielskiegoApplication.java
@@ -0,0 +1,13 @@
+package com.example.nauka_angielskiego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaukaAngielskiegoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaukaAngielskiegoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nauka_angielskiego/NaukaAngielskiegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nauka_angielskiego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaukaAngielskiegoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
